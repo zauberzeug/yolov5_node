fe521072bbbfefb16de9c10a8af465042fd08673
Rodja Trappe <rodja@zauberzeug.com>
2021-12-27 09:00:49 +0100

    showing hostname

--- detector/main.py
+++ detector/main.py
@@ -8,9 +8,8 @@ icecream.install()
 
 logging.getLogger().setLevel(logging.INFO)
 
-
 detector = Yolov5Detector()
-node = DetectorNode(name='Yolov5 detector ' + os.uname()[1], detector=detector)
+node = DetectorNode(name=os.uname()[1], detector=detector)
 
 if __name__ == "__main__":
     uvicorn.run("main:node", host="0.0.0.0", port=80, lifespan='on', reload=True)
