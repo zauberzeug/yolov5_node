8840718e54fceb31436ed8d43331c13afac5c51f
Philipp Glahe <philipp@zauberzeug.com>
2022-04-11 13:15:37 +0000

    #525: Prepare detector on init

--- detector/yolov5_detector.py
+++ detector/yolov5_detector.py
@@ -1,3 +1,4 @@
+import shutil
 from typing import Any, List
 from learning_loop_node import ModelInformation, Detector
 from learning_loop_node.detector import Detections, BoxDetection, PointDetection
@@ -9,6 +10,10 @@ import re
 import numpy as np
 import time
 import torch
+import sys
+from utils.torch_utils import select_device, time_sync
+from models.common import DetectMultiBackend
+from utils.general import check_img_size, non_max_suppression
 
 
 class Yolov5Detector(Detector):
@@ -18,21 +23,20 @@ class Yolov5Detector(Detector):
 
     def init(self,  model_info: ModelInformation, model_root_path: str):
         weightfile = f'{model_root_path}/model.pt'
-        subprocess.run(
-            f'python3 /yolov5/export.py --device 0 --half --weights {weightfile} --include engine', shell=True)
-        # self.device = select_device('0')
-        # engine_file = export(weightfile, device=self.device, half=True)
-        # logging.info(engine_file)
-        pass
-        # subprocess.run(
-        #     f'python3 /yolov5/export.py --device 0 --half --weights {weightfile} --include engine', shell=True)
-        # logging.info(model_root_path)
-        # self.model = TensorRTBackend(
-        #     f'{model_root_path}/model.engine', device=self.device, data=f'{model_root_path}/hyp.yaml')
-        # imgz = (model_info.resolution, model_info.resolution)
-        # self.imgz = check_img_size(imgz, s=self.model.stride)
-        # self.half = self.model.engine and self.device.type != 'cpu'
-        # self.model.warmup(imgz=(1, 3, *self.imgz), half=self.half)
+        if not os.path.exists(f'{model_root_path}/model.engine'):
+            shutil.copy('/data/model.engine', f'{model_root_path}/model.engine')
+            # subprocess.run(
+            #     f'python3 /yolov5/export.py --device 0 --half --weights {weightfile} --include engine', shell=True)
+        self.device = select_device('0')
+
+        self.model = DetectMultiBackend(
+            f'{model_root_path}/model.engine', device=self.device)
+        logging.info(model_info.resolution)
+        imgz = (model_info.resolution, model_info.resolution)
+        self.imgz = check_img_size(imgz, s=self.model.stride)
+        logging.info(self.imgz)
+        self.half = self.model.engine and self.device.type != 'cpu'
+        self.model.warmup(imgsz=(1, 3, *self.imgz), half=self.half)
 
     def evaluate(self, image: List[np.uint8]) -> Detections:
         detections = Detections()
