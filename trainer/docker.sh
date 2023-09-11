e19d0a7d574854de08f8f61947c5668b478b54c1
Dennis Wittich <dennis@zauberzeug.com>
2023-09-11 13:16:47 +0200

    update docker.sh

--- trainer/docker.sh
+++ trainer/docker.sh
@@ -49,7 +49,7 @@ run_args="-it --rm"
 run_args+=" -v $(pwd)/../:/yolov5_node/"
 run_args+=" -v $HOME/data:/data"
 if [ "$LINKLL" == "TRUE" ]; then
-    run_args+=" -v $HOME/learning_loop_node/learning_loop_node:/opt/conda/lib/python3.10/site-packages/learning_loop_node"
+    run_args+=" -v $HOME/learning_loop_node/learning_loop_node:/usr/local/lib/python3.10/site-packages/learning_loop_node"
     run_args+=" -v $HOME/learning_loop_node:/learning_loop_node"
 fi
 run_args+=" -v $HOME/.vscode-server:/root/.vscode-server"
