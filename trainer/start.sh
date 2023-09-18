c6f1f2358912025d7b7236012027901499797f24
Dennis Wittich <dennis@zauberzeug.com>
2023-09-18 19:13:21 +0200

    change entry point for tests

--- trainer/start.sh
+++ trainer/start.sh
@@ -5,7 +5,8 @@ if [[ $1 = "dev" ]]; then
 elif [[ $1 = "debug" ]]; then
    python3 -m debugpy --listen 5678 /app/main.py
 else
-    # only use one worker until we add a message quque (https://trello.com/c/qpgsAx9y/46-deployment-auf-learning-loopai#comment-5fe6d7d9ef84285e09cd10dc)
-    export MAX_WORKERS=1
-    gunicorn -k "uvicorn.workers.UvicornWorker" "main:node"
+   python3 /app/main.py
+#    # only use one worker until we add a message quque (https://trello.com/c/qpgsAx9y/46-deployment-auf-learning-loopai#comment-5fe6d7d9ef84285e09cd10dc)
+#    export MAX_WORKERS=1
+#    gunicorn -k "uvicorn.workers.UvicornWorker" "main:node"
 fi
