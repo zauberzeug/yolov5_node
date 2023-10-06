290e9932957c7452d1afde452c47c8678a044e90
NiklasNeugebauer <niklas@neugebauer.dev>
2023-10-06 12:32:46 +0200

    delete image if it cannot be loaded by yolo

--- trainer/app_code/yolov5/utils/dataloaders.py
+++ trainer/app_code/yolov5/utils/dataloaders.py
@@ -155,6 +155,13 @@ def create_dataloader(path,
                   generator=generator), dataset
 
 
+def load_image_delete_if_corrupt(path):
+    try:
+        return cv2.imread(path)  # BGR
+    except Exception:
+        os.remove(path)
+        raise Exception(f'Image could not be loaded and will be deleted as it is likely corrupted ({path})')
+
 class InfiniteDataLoader(dataloader.DataLoader):
     """ Dataloader that reuses workers
 
@@ -306,7 +313,7 @@ class LoadImages:
         else:
             # Read image
             self.count += 1
-            im0 = cv2.imread(path)  # BGR
+            im0 = load_image_delete_if_corrupt(path)
             assert im0 is not None, f'Image Not Found {path}'
             s = f'image {self.count}/{self.nf} {path}: '
 
@@ -733,7 +740,7 @@ class LoadImagesAndLabels(Dataset):
             if fn.exists():  # load npy
                 im = np.load(fn)
             else:  # read image
-                im = cv2.imread(f)  # BGR
+                im = load_image_delete_if_corrupt(f)
                 assert im is not None, f'Image Not Found {f}'
             h0, w0 = im.shape[:2]  # orig hw
             r = self.img_size / max(h0, w0)  # ratio
@@ -1193,7 +1200,7 @@ class ClassificationDataset(torchvision.datasets.ImageFolder):
                 np.save(fn.as_posix(), cv2.imread(f))
             im = np.load(fn)
         else:  # read image
-            im = cv2.imread(f)  # BGR
+            im = load_image_delete_if_corrupt(f)
         if self.album_transforms:
             sample = self.album_transforms(image=cv2.cvtColor(im, cv2.COLOR_BGR2RGB))["image"]
         else:
