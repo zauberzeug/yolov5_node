3fab7940decb2547f8c1bcf2b868c2d1ac2d3973
Dr. Dennis Wittich <dennis@zauberzeug.com>
2025-11-11 17:31:22 +0100

    Fix dockerfile and remove start.sh (optimize docker layers)

--- detector/start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ $1 = "debug" ]]; then
-   /app/.venv/bin/python -m debugpy --listen 5678 /app/main.py
-elif [[ $1 = "catchsegv" ]]; then
-   catchsegv /app/.venv/bin/python /app/main.py
-else
-   /app/.venv/bin/python /app/main.py
-fi
