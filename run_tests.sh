7e394e1975ac7097527fd409ff84a6b00c7ba017
Dr. Dennis Wittich <dennis@zauberzeug.com>
2025-11-26 16:27:32 +0100

    fix detector tests

--- run_tests.sh
+++ run_tests.sh
@@ -6,14 +6,14 @@ set -o allexport; source .env; set +o allexport
 # Check if argument is provided
 if [ $# -eq 1 ]; then
     # Run tests with filter
-    python -m pytest trainer -v -s -k "$1"
-    python -m pytest detector -v -s -k "$1" 
-    python -m pytest detector_cpu -v -s -k "$1"
+    ( cd python && uv run pytest trainer -v -s -k "$1" )
+    ( cd detector && uv run python -m pytest -v -s -k "$1" )
+    ( cd detector_cpu &&  uv run python -m pytest -v -s -k "$1" )
     exit 0
 fi
 
 
 # Run the tests
-cd trainer && uv run python -m pytest -v
-# python -m pytest detector -v
-# python -m pytest detector_cpu -v
+# ( cd trainer && uv run python -m pytest -v )
+# ( cd detector && uv run python -m pytest -v )
+( cd detector_cpu && uv run python -m pytest -v )
